99cdea6ae5fe7225f4e6b69586708038a647dd1d
lajdjajsh <doglhzxsssz@163.com>
2026-06-16 20:17:35 +0800

    add

---
+++ assets/FuelActTwo-BvTnwilr.js
@@ -0,0 +1,1 @@
+import{F as o}from"./FuelStoryStage-BNrbbTyN.js";import{d as t,c as e,o as r}from"./index-DVsEGPsU.js";import"./OrbitControls-B7G50aqf.js";import"./index-xgxdCp6f.js";import"./useVoice-ClOsHrD-.js";const u=t({__name:"FuelActTwo",setup(a){return(c,p)=>(r(),e(o,{act:"2"}))}});export{u as default};
---
+++ assets/FuelCore-qWLOz87O.js
@@ -0,0 +1,1 @@
+import{F as o}from"./FuelStoryStage-BNrbbTyN.js";import{d as e,c as r,o as t}from"./index-DVsEGPsU.js";import"./OrbitControls-B7G50aqf.js";import"./index-xgxdCp6f.js";import"./useVoice-ClOsHrD-.js";const u=e({__name:"FuelCore",setup(a){return(c,p)=>(t(),r(o,{act:"core"}))}});export{u as default};
